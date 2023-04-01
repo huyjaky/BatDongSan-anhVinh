23900e20836f53ebe7e1f969cafaaa83665b4d2c
huyjaky <huyphanuyg@gmail.com>
2023-04-01 16:47:34 +0700

    xac thuc duoc ssl

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const viewEngine = require('./config/viewEngine');
 const router = require('./routers/webAPI');
 const cors = require('cors');
 require('dotenv').config();
+const https = require('https');
+const fs = require('fs');
 
 let app = express();
 app.use(cors());
@@ -16,7 +18,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 viewEngine(app);
 router(app);
 
+const option = {
+  key: fs.readFileSync('I://newjava//JavaScript//BatDongSan//back-end//src//server_key.key'),
+  cert: fs.readFileSync('I://newjava//JavaScript//BatDongSan//back-end//src//server.crt')
+}
+
+const server = https.createServer(option, app);
+
 let port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log('Connected!', port);
+server.listen(port, () => {
+  console.log('Server listening on port ', port);
 });
